4d09e7c0e610b9a58015de78d1ee9452d581d7c4
Noemi Roldan <noemi.roldan@peopleshores.com>
2026-02-13 22:09:03 -0500

    Update styles.css

--- ui.js
+++ ui.js
@@ -1,8 +1,8 @@
 // ui.js
 export function createBreedCard(breed) {
   return `
-    <div class="col-md-4 mb-4">
-      <div class="card h-100" style="width: 22rem;">
+    <div class="col-12 col-sm-6 col-md-4 mb-4">
+      <div class="card h-100 justify-content-center shadow border-0" style="width: 22rem;">
         <div style="position: relative; padding-top: 100%; overflow: hidden;">
           <img src="${breed.image.url}" id="${breed.image.id}" 
                class="card-img-top w-100 h-100 position-absolute top-0 start-0" 
